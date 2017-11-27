7b7adcd8674784862dcb7391eee39dceaeef8136
Smite Chow <xiaopengyou2no1@gmail.com>
2017-11-27 22:29:17 +0800

    media to small if exists

--- calculate_weibo_clip.js
+++ calculate_weibo_clip.js
@@ -1,3 +1,8 @@
+var toSmallBtn = document.querySelector('[action-type="feed_list_media_toSmall"]');
+
+if (toSmallBtn)
+    toSmallBtn.click();
+
 var detailClip = document.querySelector('.WB_feed_detail').getBoundingClientRect();
 var handleClip = document.querySelector('.WB_feed_handle').getBoundingClientRect();
 var clip = {
